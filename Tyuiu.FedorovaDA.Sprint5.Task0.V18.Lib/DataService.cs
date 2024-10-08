abbf3c431b94a0cc18b9ef325cdf169970d148ad
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-08 17:19:35 +0500

    Add project files.

---
+++ Tyuiu.FedorovaDA.Sprint5.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FedorovaDA.Sprint5.Task0.V18.Lib
+{
+    public class DataService : ISprint5Task0V18
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double res = x / Math.Sqrt(Math.Pow(x, 2) + x);
+            res = Math.Round(res, 2);
+            File.WriteAllText(path, Convert.ToString(res));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.FedorovaDA.Sprint5.Task0.V18/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.FedorovaDA.Sprint5.Task0.V18.Lib;
+namespace Tyuiu.FedorovaDA.Sprint5.Task0.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Федрова Д. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Федорова Дарья Артемовна | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3,                        *");
+            Console.WriteLine("* результат сохранить в текстовый файл OutPutFileTask0.txt                *");
+            Console.WriteLine("* и вывести на консоль. Округлить до трёх знаков после запятой.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            Console.WriteLine("X = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
