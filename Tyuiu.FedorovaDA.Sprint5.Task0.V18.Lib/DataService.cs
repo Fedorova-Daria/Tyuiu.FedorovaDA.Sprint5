7eb1b75f81e2f2e1d44d291db5a1632850980cc6
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-09 10:30:42 +0500

    Task0

--- Tyuiu.FedorovaDA.Sprint5.Task0.V18.Lib/DataService.cs
+++ Tyuiu.FedorovaDA.Sprint5.Task0.V18.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.FedorovaDA.Sprint5.Task0.V18.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
             double res = x / Math.Sqrt(Math.Pow(x, 2) + x);
             res = Math.Round(res, 3);
             File.WriteAllText(path, Convert.ToString(res));
