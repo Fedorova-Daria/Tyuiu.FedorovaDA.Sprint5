fd5272897ce2584c6baaac077857f2614d8349f9
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-13 18:30:36 +0500

    Task3

---
+++ Tyuiu.FedorovaDA.Sprint5.Task3.V27.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text;
+namespace Tyuiu.FedorovaDA.Sprint5.Task3.V27.Lib
+{
+    public class DataService : ISprint5Task3V27
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double res = Math.Pow(x - 1, 3*x + 1);
+            res = Math.Round(res, 3);
+
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
+
+            
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(res));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.FedorovaDA.Sprint5.Task3.V27/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.FedorovaDA.Sprint5.Task3.V27.Lib;
+namespace Tyuiu.FedorovaDA.Sprint5.Task3.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Федрова Д. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнила: Федорова Дарья Артемовна | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3,                        *");
+            Console.WriteLine("* результат сохранить в бинарный файл OutPutFileTask3.bin                 *");
+            Console.WriteLine("* и вывести на консоль. Округлить до трёх знаков после запятой.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+        }
+    }
+}
