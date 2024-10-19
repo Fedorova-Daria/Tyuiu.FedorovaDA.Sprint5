2adaf31a5aef24c649b3e11397da76717afaabd3
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-19 19:24:12 +0500

    Task6

--- Tyuiu.FedorovaDA.Sprint5.Task6.V30.Lib/DataService.cs
+++ Tyuiu.FedorovaDA.Sprint5.Task6.V30.Lib/DataService.cs
@@ -10,21 +10,23 @@ namespace Tyuiu.FedorovaDA.Sprint5.Task6.V30.Lib
     {
         public int LoadFromDataFile(string path)
         {
-            
+            int count = 0;
 
             using(StreamReader reader = new StreamReader(path))
             {
-                
-
-                string[] words = path.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                int count = 0;
-
-                foreach (string word in words)
+                string line;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    if (word.Length == 8)
+                    string[] words = path.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string word in words)
                     {
-                        count++;
+                        if (word.Length == 8)
+                        {
+                            count++;
+                        }
                     }
+                    
                 }
                 return count;
             }
