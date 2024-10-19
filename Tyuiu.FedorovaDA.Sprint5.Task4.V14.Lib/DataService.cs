d90759cd945de02d6764efe4269ec18813393917
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-19 16:14:26 +0500

    Task4

--- Tyuiu.FedorovaDA.Sprint5.Task4.V14.Lib/DataService.cs
+++ Tyuiu.FedorovaDA.Sprint5.Task4.V14.Lib/DataService.cs
@@ -1,18 +1,24 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
-using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 namespace Tyuiu.FedorovaDA.Sprint5.Task4.V14.Lib
 {
     public class DataService : ISprint5Task4V14
     {
         public double LoadFromDataFile(string path)
         {
+            bool check = false;
+
             using (StreamReader reader = new StreamReader(path))
             {
                 string line = reader.ReadLine();
                 double x = int.Parse(line);
-                double res = Math.Round(Math.Sin(Math.Pow(x, 3)) + 2 / x, 3);
-                return res;
+                return Math.Round(Math.Sin(Math.Pow(x, 3)) + 2 / x, 3);
             }
 
         }
